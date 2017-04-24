58398ffb0aa4d0ca8a75252d61881d6b82c0325b
micafinn <micafinn@gmail.com>
2017-04-24 15:24:19 +0200

    Also use ':limit => 20' when using Task class method in the projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
-    @tasks = @project.tasks.find_incomplete
+    @tasks = @project.tasks.find_incomplete :limit => 20 
   end
 end
