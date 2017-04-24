6e7afbc658dd93b33841194d6b0afd655fe3f738
micafinn <micafinn@gmail.com>
2017-04-24 17:02:31 +0200

    Expeimetn adding more custom made controller layouts to the project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ApplicationController
+  layout "admim"
   def show
     @project = Project.find(params[:id])
-    @tasks = @project.tasks.find_incomplete :limit => 20 
+    @tasks = @project.tasks.find_incomplete :limit => 20
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,8 @@ class Task < ApplicationRecord
   belongs_to :model
 
   def self.find_incomplete(options = {})
-    find_all_by_complete(false, :order => 'created_at DESC')
+    with_scope :find => options do
+      find_all_by_complete(false, :order => 'created_at DESC')
+    end 
   end
 end
