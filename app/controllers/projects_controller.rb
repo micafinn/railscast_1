4d74c428f3a5fa363de3d81bcee6dda4028266d2
micafinn <micafinn@gmail.com>
2017-04-24 17:08:33 +0200

    Add dynamic layout finder

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,18 @@
 class ProjectsController < ApplicationController
-  layout "admin"
+  layout :user_layout
+
   def show
     @project = Project.find(params[:id])
     @tasks = @project.tasks.find_incomplete :limit => 20
+  end
+
+  protected
+
+  def user_layout
+    if current_user.admin? #can just say => if true, in order to avoid an error of this undefined method
+      "admin"
+    else
+      "application"
+    end
   end
 end
