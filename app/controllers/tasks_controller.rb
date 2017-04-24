e24c5ec141f84e8d958b4131d57e785c5218b1f4
micafinn <micafinn@gmail.com>
2017-04-24 14:23:37 +0200

    Generate git controller and add incomplete and last_incomplete actions

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,9 @@
+class TasksController < ApplicationController
+  def incomplete
+    @tasks - Task.find(:all, :conditions => ['complete = ?', false])
+  end
+
+  def last_incomplete
+    @task = Task.find(:first, :conditions => ['complete = ?', false], :order => 'create_at Desc')
+  end
+end
