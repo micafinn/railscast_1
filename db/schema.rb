09ac133ca91c3433a490913cd9012059be15bff3
micafinn <micafinn@gmail.com>
2017-04-24 14:32:34 +0200

    Generate task model and associate it with project model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424123016) do
+ActiveRecord::Schema.define(version: 20170424123131) do
 
   create_table "projects", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
