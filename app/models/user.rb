b7dda75e7ee4a7ec908bd1f435f4185e626ad34f
micafinn <micafinn@gmail.com>
2017-04-25 14:06:28 +0200

    create a new method to render the period after the middlename initial

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
 
   def full_name
 
-    [first_name, middle_initial, last_name].compact.join(' ')
-    name = first_name + " "
-    name += "#{middle_initial}. " unless middle_initial.nil?
-    name += last_name
-    name
+    [first_name, middle_initial_with period, last_name].compact.join(' ')
+  end
+
+  def middle_initial_with_period
+    "#{middle_initial}." unless middle_initial.nil?
   end
 end
