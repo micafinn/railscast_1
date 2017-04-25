15db3132e3b185ff8ad7ca4682fffd2be578235f
micafinn <micafinn@gmail.com>
2017-04-25 12:18:34 +0200

    Add 2 tests to the user_test.rb file, and install autotest

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :users
+
+  def test_full_name_without_middle_initial
+    user = User.new(:first_name => 'John', :last_name => 'Doe')
+    assert_equal 'John Doe', user.full_name
+  end
+
+  def test_full_name_with_middle_initial
+    user = User.new(:first_name => 'John', :middle_initial =>'H', :last_name => 'Doe')
+    assert_equal 'John H. Doe', user.full_name
+  end
 end
