12baf54492f53d7198c23067f101ecac49cbdde4
micafinn <micafinn@gmail.com>
2017-04-25 14:00:08 +0200

    Refactor method including the .compact directive, as one of the tests fails due to poor spacing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
 
   def full_name
+
+    [first_name, middle_initial, last_name].compact.join(' ')
     name = first_name + " "
     name += "#{middle_initial}. " unless middle_initial.nil?
     name += last_name
