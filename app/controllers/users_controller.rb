fe10bca35996a9cb497e2ca6c012cc6f7bcdde1e
micafinn <micafinn@gmail.com>
2017-04-25 14:47:13 +0200

    Wrongly create a session for the first user (hardcoded) inside the model, and add a views template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def prepare
+    session[:user] = User.find(:first)
+    redirect_to :action => 'show'
+  end
+
+  def show
+    @user = session[:user]
+  end
+
+  def update
+    @user = session[:user]
+    @user.name = "Foo"
+    redirect_to :action => 'show' 
+  end
 end
