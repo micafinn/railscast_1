77a7bfb0c99c7a69f49d47d97171083dea6e4f06
micafinn <micafinn@gmail.com>
2017-04-25 14:09:04 +0200

    Add test to check output when middle initial is left blank

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,4 +12,9 @@ class UserTest < ActiveSupport::TestCase
     user = User.new(:first_name => 'John', :middle_initial =>'H', :last_name => 'Doe')
     assert_equal 'John H. Doe', user.full_name
   end
+
+  def test_full_name_with_blank_middle_initial
+    user = User.new(:first_name => 'John', :middle_initial =>'', :last_name => 'Doe')
+    assert_equal 'John H. Doe', user.full_name
+  end
 end
